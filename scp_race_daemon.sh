ae9bda886c986180ce683f69718f8aad3ec456ec
Marcos Felipe <mfelipe11@yahoo.com.br>
2018-04-10 17:38:23 -0300

    Add class builder

---
+++ scp_race_daemon.sh
@@ -0,0 +1,3 @@
+#scp -r ./. root@10.16.0.208:/opt/ater/race/race-daemon-2/src
+#scp -r ./. root@10.16.0.202:/opt/ater/race/testes
+scp -P 3001 -r ./. root@200.130.99.253:/opt/ater/race/testes
--- testes_of_13.py
+++ testes_of_13.py
@@ -4,6 +4,7 @@ from ryu.ofproto import ofproto_v1_3_parser as ofp
 from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.topology import event
+import copy
 
 class RuleCreator(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
@@ -13,25 +14,28 @@ class RuleCreator(app_manager.RyuApp):
 
     def add_flow(self, datapath):
         inst = [ofp.OFPInstructionActions(ofproto_v1_3.OFPIT_APPLY_ACTIONS, self.create_actions())]
-        match = self.create_match()
-        mod = ofp.OFPFlowMod(datapath=datapath, match=match, instructions=inst)
-        datapath.send_msg(mod)
+        match_list = self.create_match()
+        for match in match_list:
+            print(match)
+            mod = ofp.OFPFlowMod(datapath=datapath, match=match, instructions=inst)
+            datapath.send_msg(mod)
 
     def create_match(self):
-        mb = Match_builder()
-        mb.set_in_port(10)
-        mb.set_eth_type(0x800)
-        mb.set_ipv4_src('192.168.0.1')
-        mb.set_ipv4_src_mask('32')
-        mb.set_ipv4_dst('192.168.0.2')
-        mb.set_ipv4_dst_mask('24')
-        mb.set_vlan_vid(100)
+        mb = MatchBuilder()
+        match_list = []
 
-        match = mb.build_normal()
-        return match
+        ports = [1,2,3]
+        match = mb.set_in_port(1).set_eth_type().set_vlan_vid((0x1000 | 101))
+
+        for p in ports:
+            match_copy = match
+            match_copy = match_copy.set_tcp_field(src_port=p).build()
+            match_list.append(match_copy)
+
+        return match_list
 
     def create_actions(self):
-        actions = [ofp.OFPActionOutput(port=10)]
+        actions = [ofp.OFPActionOutput(port=3)]
         return  actions
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
@@ -39,144 +43,55 @@ class RuleCreator(app_manager.RyuApp):
         datapath = ev.msg.datapath
         self.add_flow(datapath)
 
-class Match_builder():
+class MatchBuilder():
 
     def __init__(self):
-        self.eth_type = None
-        self.ipv4_src = None
-        self.ipv4_dst = None
-        self.in_port  = None
-        self.vlan_id  = None
-        self.ipv4_src_mask = None
-        self.ipv4_dst_mask = None
+        self.match_fields = {}
 
-        self.src_port = None
-        self.dst_port = None
+    def set_eth_type(self, eth_type=0x800):
+        self.match_fields['eth_type'] = eth_type
+        return self
 
-    def build(self):
-        pass
+    def set_ipv4_src(self, ipv4_src, ipv4_src_mask='32'):
+        mask = '255.255.255.255'
+        if ipv4_src_mask == '24':
+            mask = '255.255.255.0'
+        self.match_fields['ipv4_src'] = (ipv4_src, mask)
+        return self
 
-    def set_eth_type(self, eth_type):
-        self.eth_type = eth_type
-
-    def set_ipv4_src(self, ipv4_src):
-        self.ipv4_src = ipv4_src
-
-    def set_ipv4_dst(self, ipv4_dst):
-        self.ipv4_dst = ipv4_dst
+    def set_ipv4_dst(self, ipv4_dst, ipv4_dst_mask='32'):
+        mask = '255.255.255.255'
+        if ipv4_dst_mask == '24':
+            mask = '255.255.255.0'
+        self.match_fields['ipv4_src'] = (ipv4_dst, mask)
+        return self
 
     def set_in_port(self, in_port):
-        self.in_port = in_port
+        self.match_fields['in_port'] = in_port
+        return self
 
     def set_vlan_vid(self, vlan_id):
-        self.vlan_id = vlan_id
+        self.match_fields['vlan_vid'] = vlan_id
+        return self
 
     def set_src_port(self, src_port):
-        self.src_port = src_port
+        self.match_fields['src_port'] = src_port
+        return self
 
     def set_dst_port(self, dst_port):
-        self.dst_port = dst_port
+        self.match_fields['dst_port'] = dst_port
+        return self
 
-    def set_ipv4_src_mask(self, ipv4_mask):
-        if ipv4_mask == '32':
-            self.ipv4_src_mask = '255.255.255.255'
-        elif ipv4_mask == '24':
-            self.ipv4_src_mask = '255.255.255.0'
+    def set_tcp_field(self, src_port=None, dst_port=None):
+        self.match_fields['ip_proto']= 6
+        if src_port is not None:
+            self.match_fields['tcp_src'] = src_port
+        if dst_port is not None:
+            self.match_fields['tcp_dst'] = dst_port
+        return self
 
-    def set_ipv4_dst_mask(self, ipv4_mask):
-        if ipv4_mask == '32':
-            self.ipv4_dst_mask = '255.255.255.255'
-        elif ipv4_mask == '24':
-            self.ipv4_dst_mask = '255.255.255.0'
-
-    def build_normal(self):
-
-        match = ofp.OFPMatch(eth_type=self.eth_type,
-                             ipv4_dst=(self.ipv4_dst, self.ipv4_dst_mask),
-                             ipv4_src=(self.ipv4_src, self.ipv4_src_mask),
-                             in_port=self.in_port,
-                             vlan_vid=self.vlan_id)
+    def build(self):
+        match = ofp.OFPMatch(**self.match_fields)
         return match
 
-    def build_icmp(self):
-        ip_proto = 1
-        match = ofp.OFPMatch(eth_type=self.eth_type,
-                             ipv4_dst=(self.ipv4_dst, self.ipv4_dst_mask),
-                             ipv4_src= (self.ipv4_src, self.ipv4_src_mask),
-                             in_port=self.in_port,
-                             vlan_vid=self.vlan_id,
-                             ip_proto=ip_proto)
-        return match
 
-    def build_tcp(self):
-        ip_proto = 6
-        if self.src_port is not None and self.dst_port is not None:
-            match = ofp.OFPMatch(eth_type=self.eth_type,
-                                 ipv4_dst=(self.ipv4_dst, self.ipv4_dst_mask),
-                                 ipv4_src=(self.ipv4_src, self.ipv4_src_mask),
-                                 in_port=self.in_port,
-                                 vlan_vid=self.vlan_id,
-                                 tcp_src= self.src_port,
-                                 tcp_dst=self.dst_port,
-                                 ip_proto=ip_proto)
-
-        elif self.src_port is not None:
-            match = ofp.OFPMatch(eth_type=self.eth_type,
-                                 ipv4_dst=(self.ipv4_dst, self.ipv4_dst_mask),
-                                 ipv4_src=(self.ipv4_src, self.ipv4_src_mask),
-                                 in_port=self.in_port,
-                                 vlan_vid=self.vlan_id,
-                                 tcp_src=self.src_port,
-                                 ip_proto=ip_proto)
-        elif self.dst_port is not None:
-            match = ofp.OFPMatch(eth_type=self.eth_type,
-                                 ipv4_dst=(self.ipv4_dst, self.ipv4_dst_mask),
-                                 ipv4_src=(self.ipv4_src, self.ipv4_src_mask),
-                                 in_port=self.in_port,
-                                 vlan_vid=self.vlan_id,
-                                 tcp_dst=self.src_port,
-                                 ip_proto=ip_proto)
-        else:
-            match = ofp.OFPMatch(eth_type=self.eth_type,
-                                 ipv4_dst=(self.ipv4_dst, self.ipv4_dst_mask),
-                                 ipv4_src=(self.ipv4_src, self.ipv4_src_mask),
-                                 in_port=self.in_port,
-                                 vlan_vid=self.vlan_id,
-                                 ip_proto=ip_proto)
-        return match
-
-    def build_upd_port(self):
-        ip_proto = 17
-        if self.src_port is not None and self.dst_port is not None:
-            match = ofp.OFPMatch(eth_type=self.eth_type,
-                                 ipv4_dst=(self.ipv4_dst, self.ipv4_dst_mask),
-                                 ipv4_src=(self.ipv4_src, self.ipv4_src_mask),
-                                 in_port=self.in_port,
-                                 vlan_vid=self.vlan_id,
-                                 udp_src=self.src_port,
-                                 udp_dst=self.dst_port,
-                                 ip_proto=ip_proto)
-        elif self.src_port is not None:
-            match = ofp.OFPMatch(eth_type=self.eth_type,
-                                 ipv4_dst=(self.ipv4_dst, self.ipv4_dst_mask),
-                                 ipv4_src=(self.ipv4_src, self.ipv4_src_mask),
-                                 in_port=self.in_port,
-                                 vlan_vid=self.vlan_id,
-                                 udp_src=self.src_port,
-                                 ip_proto=ip_proto)
-        elif self.dst_port is not None:
-            match = ofp.OFPMatch(eth_type=self.eth_type,
-                                 ipv4_dst=(self.ipv4_dst, self.ipv4_dst_mask),
-                                 ipv4_src=(self.ipv4_src, self.ipv4_src_mask),
-                                 in_port=self.in_port,
-                                 vlan_vid=self.vlan_id,
-                                 udp_dst=self.src_port,
-                                 ip_proto=ip_proto)
-        else:
-            match = ofp.OFPMatch(eth_type=self.eth_type,
-                                 ipv4_dst=(self.ipv4_dst, self.ipv4_dst_mask),
-                                 ipv4_src=(self.ipv4_src, self.ipv4_src_mask),
-                                 in_port=self.in_port,
-                                 vlan_vid=self.vlan_id,
-                                 ip_proto=ip_proto)
-        return match
