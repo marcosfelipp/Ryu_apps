547970b79e893f0307f4be44d24e5aedb3f69eeb
Marcos Felipe <mfelipe11@yahoo.com.br>
2018-04-10 17:39:51 -0300

    Add class Builder

--- README.md
+++
@@ -1,2 +0,0 @@
-# Ryu_apps
-Creates a simple Ryu app using the tutorials and then adds on to it.
--- scp_race_daemon.sh
+++
@@ -1,3 +0,0 @@
-#scp -r ./. root@10.16.0.208:/opt/ater/race/race-daemon-2/src
-#scp -r ./. root@10.16.0.202:/opt/ater/race/testes
-scp -P 3001 -r ./. root@200.130.99.253:/opt/ater/race/testes
