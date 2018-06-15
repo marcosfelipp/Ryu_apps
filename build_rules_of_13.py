dbf22e40488b84cbd4d2ce1067b195e0bb39b9c4
marcosfelipp <mfelipe11@yahoo.com.br>
2018-06-15 13:35:12 -0300

    Add action normal and swich port

--- build_rules_of_13.py
+++ build_rules_of_13.py
@@ -3,8 +3,7 @@ from ryu.ofproto import ofproto_v1_3
 from ryu.ofproto import ofproto_v1_3_parser as ofp
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.topology import event
-import copy
+
 
 class RuleCreator(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
@@ -23,24 +22,21 @@ class RuleCreator(app_manager.RyuApp):
 
     def create_actions(self):
         actions_builder = Actions13Builder()
-        actions = actions_builder.set_output_port(10).set_vlan(10).build()
+        actions = actions_builder.set_output_normal_port().set_controller_output_port().set_pop_vlan().build()
         return actions
 
     def create_match(self):
-        '''
+        """
         Create math with multiple TCP ports:
-        :return:
-        '''
+        :return: match list
+        """
         mb = MatchBuilder()
         match_list = []
 
-        ports = [1,2,3]
-        match = mb.set_eth_type().set_in_port(1).set_ipv4_dst('10.1.0.1').set_ipv4_src("10.2.0.1").set_vlan_vid((0x1000 | 101))
+        match = mb.set_eth_type().set_in_port(1).set_ipv4_dst('10.1.0.1').set_ipv4_src("10.2.0.1").\
+            set_vlan_vid((0x1000 | 101)).set_tcp_field(src_port=1).build()
 
-        for p in ports:
-            match_copy = match
-            match_copy = match_copy.set_udp_field(src_port=p).build()
-            match_list.append(match_copy)
+        match_list.append(match)
 
         return match_list
 
@@ -49,7 +45,8 @@ class RuleCreator(app_manager.RyuApp):
         datapath = ev.msg.datapath
         self.add_flow(datapath)
 
-class MatchBuilder():
+
+class MatchBuilder:
 
     def __init__(self):
         self.match_fields = {}
@@ -89,7 +86,7 @@ class MatchBuilder():
         return self
 
     def set_tcp_field(self, src_port=None, dst_port=None):
-        self.match_fields['ip_proto']= 6
+        self.match_fields['ip_proto'] = 6
         if src_port is not None:
             self.match_fields['tcp_src'] = src_port
         if dst_port is not None:
@@ -103,10 +100,11 @@ class MatchBuilder():
         if dst_port is not None:
             self.match_fields['udp_dst'] = dst_port
         return self
-		
+
     def build(self):
         match = ofp.OFPMatch(**self.match_fields)
         return match
+
 
 class Actions13Builder(object):
 
@@ -126,13 +124,21 @@ class Actions13Builder(object):
         self.actions_fields.append(ofp.OFPActionPopVlan())
         return self
 
-    def push_vlan(self):
-        self.actions_fields.append(ofp.OFPActionPushVlan())
+    def set_pop_vlan(self):
+        self.actions_fields.append(ofp.OFPActionPopVlan())
+        return self
 
     def set_output_port(self, port):
         self.actions_fields.append(ofp.OFPActionOutput(port=port))
         return self
 
+    def set_output_normal_port(self):
+        self.actions_fields.append(ofp.OFPActionOutput(ofproto_v1_3.OFPP_NORMAL))
+        return self
+
+    def set_controller_output_port(self):
+        self.actions_fields.append(ofp.OFPActionOutput(ofproto_v1_3.OFPP_CONTROLLER))
+        return self
+
     def build(self):
         return [ofp.OFPInstructionActions(ofproto_v1_3.OFPIT_APPLY_ACTIONS, self.actions_fields)]
-
